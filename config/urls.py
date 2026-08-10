2b91d2a6524bbe88081545ab2ae31f8bc5d23479
site-open <brypielago@gmail.com>
2026-08-10 12:30:17 +0800

    Customize Django admin site branding
    Updated Django admin site header, title, and index title.

--- config/urls.py
+++ config/urls.py
@@ -3,6 +3,24 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 
-urlpatterns = [path('admin/', admin.site.urls), path('', include('core.urls'))]
+
+# =========================
+# CUSTOM DJANGO ADMIN NAME
+# =========================
+
+admin.site.site_header = "Barangay Maniwaya"
+admin.site.site_title = "Barangay Maniwaya Admin Portal"
+admin.site.index_title = "Barangay Management System"
+
+
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("", include("core.urls")),
+]
+
+
 if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(
+        settings.MEDIA_URL,
+        document_root=settings.MEDIA_ROOT
+    )
