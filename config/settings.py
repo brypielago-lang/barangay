cc914e4203f67f52dcc18f977bd8cc40e235df66
site-open <brypielago@gmail.com>
2026-08-10 19:55:31 +0800

    Update core app configuration in settings

--- config/settings.py
+++ config/settings.py
@@ -12,7 +12,7 @@ ALLOWED_HOSTS = ['*']
 INSTALLED_APPS = [
     'django.contrib.admin', 'django.contrib.auth', 'django.contrib.contenttypes',
     'django.contrib.sessions', 'django.contrib.messages', 'django.contrib.staticfiles',
-    'core',
+    'core.apps.CoreConfig',
 ]
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
