6af8472df18ab5f82b648cff592b75638274ae6b
site-open <brypielago@gmail.com>
2026-08-11 02:03:06 +0800

    Add email configuration to settings.py

--- config/settings.py
+++ config/settings.py
@@ -77,6 +77,14 @@ LOGOUT_REDIRECT_URL = 'login'
 LOGIN_URL = 'login'
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
+
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 STORAGES = {
     'default': {
         'BACKEND': 'cloudinary_storage.storage.MediaCloudinaryStorage',
